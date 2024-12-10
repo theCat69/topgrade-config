e9e2e2a37fc8a76d92812b9f1a3de94da6563f6b
Felix Vadcard <vadcard.felix@gmail.com>
2024-12-10 21:08:41 +0100

    adding release tag to nvim build

--- update-scripts/update_neovim.py
+++ update-scripts/update_neovim.py
@@ -5,9 +5,9 @@ import update_lib
 def build_neovim():
     if os.name == 'nt': 
         subprocess.run(["cmake", "-S",  "cmake.deps", "-B" , ".deps" , "-G", "Ninja",  "-D", "CMAKE_BUILD_TYPE=Release"])
-        subprocess.run(["cmake", "--build",  ".deps"])
+        subprocess.run(["cmake", "--build",  ".deps", "--config", "Release"])
         subprocess.run(["cmake", "-B",  "build", "-G" , "Ninja" , "-D", "CMAKE_INSTALL_PREFIX=C:\\nvim", "-D", "CMAKE_BUILD_TYPE=Release"])
-        subprocess.run(["cmake", "--build",  "build"])
+        subprocess.run(["cmake", "--build",  "build", "--config", "Release"])
     elif os.name == 'posix':
         subprocess.run(["make", "CMAKE_BUILD_TYPE=Release"])
         subprocess.run(["sudo", "make", "install"])
