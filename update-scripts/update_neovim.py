194bf86d91accf5b44d7839ef9cf8f4d01c0e2a7
Felix Vadcard <vadcard.felix@gmail.com>
2024-12-25 20:29:07 +0100

    adding back all neovim commands and cleaning during build

--- update-scripts/update_neovim.py
+++ update-scripts/update_neovim.py
@@ -4,12 +4,16 @@ import update_lib
 
 def build_neovim():
     if os.name == 'nt': 
+        subprocess.run(["rm", "-rf", ".deps"])
+        subprocess.run(["rm", "-rf", "build"])
         subprocess.run(["cmake", "-S",  "cmake.deps", "-B" , ".deps" , "-G", "Ninja",  "-D", "CMAKE_BUILD_TYPE=Release"])
         subprocess.run(["cmake", "--build",  ".deps", "--config", "Release"])
         subprocess.run(["cmake", "-B",  "build", "-G" , "Ninja" , "-D", "CMAKE_INSTALL_PREFIX=C:\\nvim", "-D", "CMAKE_BUILD_TYPE=Release"])
         subprocess.run(["cmake", "--build",  "build", "--config", "Release"])
         subprocess.run(["cmake", "--install", "build"])
     elif os.name == 'posix':
+        subprocess.run(["rm", "-rf", ".deps"])
+        subprocess.run(["rm", "-rf", "build"])
         subprocess.run(["make", "CMAKE_BUILD_TYPE=Release"])
         subprocess.run(["sudo", "make", "install"])
 
